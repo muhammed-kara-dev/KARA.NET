aeaa0a9187a5cb823e78f69deb2cc8f532becc10
Muhammed Kara <m@kara-dev.com>
2024-09-21 20:38:30 +0200

    init

--- KPM.Server.Web/Program.cs
+++ KPM.Server.Web/Program.cs
@@ -1,10 +1,6 @@
 using KARA.NET.AspNet;
 using KARA.NET.Data.EntityFramework;
-using KPM.Business;
 using KPM.Server.Web.Components;
-
-// facade wird nicht registriert
-// automapper funktioniert nicht
 
 KARA.NET.App.AddAssemblies("KARA.NET", "KPM");
 
@@ -12,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile($"appsettings.{Environment.MachineName}.json", optional: true, reloadOnChange: true);
 builder.Services.Configure<List<DatabaseSettings>>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
 builder.Services.AddRazorComponents();
-builder.Services.AddScoped<PasswordFacade>(); // TODO facade
 ServiceManager.Register(builder.Services);
 
 var app = builder.Build();
