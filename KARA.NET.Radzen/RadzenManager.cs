f772b7a9d2c87140b891de86816585e8e2a56c95
Muhammed Kara <m@kara-dev.com>
2024-09-22 12:05:16 +0200

    radzen

---
+++ KARA.NET.Radzen/RadzenManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Radzen;
+
+namespace KARA.NET.Radzen;
+public static class RadzenManager
+{
+    public static void Register(IServiceCollection services)
+    {
+        services.AddRadzenComponents();
+    }
+}
--- KPM.Server.Web/Program.cs
+++ KPM.Server.Web/Program.cs
@@ -1,16 +1,17 @@
 using KARA.NET.AspNet;
 using KARA.NET.Data.EntityFramework;
+using KARA.NET.Radzen;
 using KPM.Server.Web.Components;
 
 // TODO logger
-// TODO radzen
 var assemblies = KARA.NET.App.AddAssemblies("KARA.NET", "KPM");
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile($"appsettings.{Environment.MachineName}.json", optional: true, reloadOnChange: true);
 builder.Services.Configure<List<DatabaseSettings>>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 ServiceManager.Register(builder.Services);
+RadzenManager.Register(builder.Services);
 
 var app = builder.Build();
 if (!app.Environment.IsDevelopment())
@@ -21,5 +22,5 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
-app.MapRazorComponents<App>().AddAdditionalAssemblies(assemblies);
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 app.Run();
